5f8aac878f900ed63e0cbd00a909dd2cbf62ba30
Alex <alexandre_2420@outlook.com>
2022-03-15 17:38:03 -0500

    add: divide method

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import { add, subtract, multiply } from './math.js'
+import { add, subtract, multiply, divide } from './math.js'
 
 
 console.log("suma de 4 + 5: " ,add(4,5))
+console.log("suma de 10 + 5: " ,add(10,5))
+console.log("suma de 4 + 12: " ,add(4,12))
+
+
 console.log("resta de 4 - 5: " ,subtract(4,5))
+console.log("resta de 14 - 4: " ,subtract(14,4))
+console.log("resta de 20 - 5: " ,subtract(20,5))
+
 
 console.log("multiply de 4 * 5: ", multiply(4, 5))
+console.log("multiply de 3 * 0: ", multiply(3, 0))
+console.log("multiply de 6 * 6: ", multiply(4, 6))
 
+
+console.log("divide de 50 / 10: ", divide(50, 10))
+console.log("divide de 12 / 4: ", divide(12, 4))
+console.log("divide de 24 / 6: ", divide(24, 6))
+console.log("divide de 12 / 0: ", divide(12, 0))
--- src/math.js
+++ src/math.js
@@ -9,3 +9,18 @@ export const multiply = (a, b) => {
     return createArrayInt(b)
         .reduce((c,n) => add(c,a),0)
 }
+
+/**
+ * Division Entera
+ * @param {number} num
+ * @param {number} den
+ * @returns {number}
+ */
+export const divide = (num, den) => {
+    if(den === 0) return 0
+
+    return createArrayInt(den)
+        .reduce((acum, number) =>
+            multiply(number, den) <= num ? acum +1 : acum )
+
+}
